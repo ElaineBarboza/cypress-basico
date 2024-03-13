26e422af9a014c8f4ebd37b859b02085345ef9a7
ElaineBarboza <159815857+ElaineBarboza@users.noreply.github.com>
2024-03-13 11:44:10 -0300

    .

--- cypress/e2e/exerciciosvini.cy.js
+++ cypress/e2e/exerciciosvini.cy.js
@@ -7,7 +7,6 @@ describe("Cadastro de Usuários", () => {
         cy.get('[href="/login"]').click();
         cy.get('.login-form').should('to.exist');
         cy.get('[data-qa="signup-name"]').type("Elaine")
-        
     })
 })
 
